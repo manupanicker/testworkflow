4115c81fe9645151e1b5e6b110e6cd3a63bc3d4d
manupanicker <panicker.manu@gmail.com>
2026-08-23 04:05:39 +0530

    Add Windows VM Terraform variables

---
+++ Terraform/variables.tf
@@ -0,0 +1,64 @@
+variable "vm_name" {
+  type        = string
+  description = "Azure VM name"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "CITRIX_BUILD"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "vnet_name" {
+  type    = string
+  default = "vnet-eastus-2"
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "snet-eastus-1"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_D2as_v7"
+}
+
+variable "dns_server" {
+  type    = string
+  default = "172.16.0.4"
+}
+
+variable "image_publisher" {
+  type    = string
+  default = "microsoftwindowsserver"
+}
+
+variable "image_offer" {
+  type    = string
+  default = "windowsserver2022"
+}
+
+variable "image_sku" {
+  type    = string
+  default = "2022-datacenter-smalldisk-g2"
+}
+
+variable "image_version" {
+  type    = string
+  default = "latest"
+}
+
+variable "os_disk_type" {
+  type    = string
+  default = "Premium_LRS"
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
